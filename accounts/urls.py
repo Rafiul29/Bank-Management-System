93472363be552348ce6c0b8ef3df6a2f8cd01cca
Rafiul29 <rafiulislam4472@gmail.com>
2024-06-10 18:58:10 +0600

    user profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/',views.UserRegistrationView.as_view(),name='register'),
     path('login/',views.UserLoginView.as_view(),name='login'),
     path('logout/',views.UserLogoutView.as_view(),name='logout'),
+    path('profile/',views.UserBankAccountUpdateView.as_view(),name='profile'),
 ]
